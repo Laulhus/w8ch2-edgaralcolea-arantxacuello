00c31ef4ce6e1d899e35e13d0cc0a29e4102305a
Arantxa Cuello <arantxa_312@hotmail.com>
2022-03-02 21:40:07 +0100

    home ssr function added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return <></>;
+export default function Home({ tuits }) {
+  return (
+    <>
+      <ul>
+        {tuits.map((tuit) => (
+          <TuitCard key={tuit.name} tuit={tuit} />
+        ))}
+      </ul>
+    </>
+  );
 }
+
+export const getServerSideProps = async () => {
+  const response = await fetch(process.env.NEXT_PUBLIC_API);
+
+  const tuitAPI = await response.json();
+  const tuits = tuitAPI.results;
+  return { props: { tuits } };
+};
