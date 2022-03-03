e3c62eabda640ef0262eacbd0d495bf9d4c473dd
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-03 10:30:03 +0100

    Modified coverage config

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,12 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
-  collectCoverageFrom: ["./**/*.js"],
+  collectCoverageFrom: [
+    "./**/*.js",
+    "!./**/*.config.js",
+    "!./**/_app.js",
+    "!coverage/**",
+  ],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
